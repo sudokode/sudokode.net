c32dedb10966decdf3b2139164e93a58ce83852f
sudokode <sudokode@gmail.com>
2015-11-28 13:06:22 -0500

    Add route for php snoopers

--- sudokode_net/__init__.py
+++ sudokode_net/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
 
 import sudokode_net.views.robots
+import sudokode_net.views.php
 import sudokode_net.views.index
 import sudokode_net.views.hello
