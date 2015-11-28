0f8ad51539743c2b3dd8dba6ec2640ef2ebc6b5e
sudokode <sudokode@gmail.com>
2015-11-28 13:10:59 -0500

    Add alert for php route

--- sudokode_net/views/php.py
+++ sudokode_net/views/php.py
@@ -6,5 +6,6 @@ from sudokode_net import app
 @app.route('/myadmin/scripts/setup.php')
 @app.route('/pma/scripts/setup.php')
 def php():
+    print("PHP lulz")
     return 'PHP? Really? The source is <a href="https://github.com/sudokode/sudokode.net">here</a>.'
 
