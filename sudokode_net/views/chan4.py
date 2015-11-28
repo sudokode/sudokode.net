0b6cdd460c6df2fdc30f81035582413bb41ded6c
sudokode <sudokode@gmail.com>
2015-11-28 15:39:56 -0500

    Add scripts and 4chan routes

---
+++ sudokode_net/views/chan4.py
@@ -0,0 +1,9 @@
+from flask import redirect
+
+from sudokode_net import app
+
+@app.route('/4chan/')
+def chan4():
+    print("4chan is here")
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
