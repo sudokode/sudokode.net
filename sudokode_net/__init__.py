103a373b7f41d7947c1dc6362cb3c9e780ed2038
sudokode <sudokode@gmail.com>
2015-11-28 15:36:17 -0500

    Fix js/css links

--- sudokode_net/__init__.py
+++ sudokode_net/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 
+import sudokode_net.views.scripts
 import sudokode_net.views.robots
 import sudokode_net.views.php
 import sudokode_net.views.chan4
