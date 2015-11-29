0042436a341b18f521f7ee665200e9f2a0092e05
sudokode <sudokode@gmail.com>
2015-11-28 22:39:29 -0500

    Bad way to load scripts

--- sudokode_net/__init__.py
+++ sudokode_net/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 
-import sudokode_net.views.scripts
 import sudokode_net.views.robots
 import sudokode_net.views.php
 import sudokode_net.views.chan4
