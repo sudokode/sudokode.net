04591cae86ca59b5c47da03f5abb48ac178fbcbe
sudokode <sudokode@gmail.com>
2016-01-07 21:30:27 -0500

    Redirect hackers to source

--- sudokode_net/views/hack.py
+++ sudokode_net/views/hack.py
@@ -5,7 +5,11 @@ from sudokode_net import app
 @app.route('/phpMyAdmin/scripts/setup.php')
 @app.route('/myadmin/scripts/setup.php')
 @app.route('/pma/scripts/setup.php')
-def php():
-    print("PHP lulz")
+@app.route('/wp-login.php')
+@app.route('/login/')
+@app.route('/admin/')
+@app.route('/administrator/')
+def hack():
+    print("hacker alert!")
     return redirect("https://github.com/sudokode/sudokode.net")
 
