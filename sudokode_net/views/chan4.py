3f1fda911b3a225c6be795b4c988e96cf9585f4e
sudokode <sudokode@gmail.com>
2016-01-07 20:06:26 -0500

    Change url

--- sudokode_net/views/chan4.py
+++ sudokode_net/views/chan4.py
@@ -5,5 +5,5 @@ from sudokode_net import app
 @app.route('/4chan/')
 def chan4():
     print("4chan is here")
-    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return redirect("https://www.youtube.com/watch?v=DLzxrzFCyOs")
 
