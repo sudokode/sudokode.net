1af461692a0b3e9513fa709f1023b795ba19777d
sudokode <sudokode@gmail.com>
2016-01-07 21:31:58 -0500

    Load new hack view

--- sudokode_net/__init__.py
+++ sudokode_net/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='static')
 hello_feed = list()
 
 import sudokode_net.views.robots
-import sudokode_net.views.php
+import sudokode_net.views.hack
 import sudokode_net.views.chan4
 import sudokode_net.views.index
 import sudokode_net.views.hello
