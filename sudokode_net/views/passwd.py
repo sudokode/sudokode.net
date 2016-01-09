98597efd5c9a993666380c43a3af36bf5ee39e32
sudokode <sudokode@gmail.com>
2016-01-09 00:48:25 -0500

    Add all the things

--- sudokode_net/views/passwd.py
+++ sudokode_net/views/passwd.py
@@ -1,11 +1,29 @@
-from flask import render_template as temp
+from flask import request, redirect, render_template as temp
 
 from sudokode_net import app
 
-def passwd_temp():
-    return temp('passwd.html', title="Password Checker")
+import passwordmeter as pwm
 
-@app.route('/passwd')
+def passwd_temp(test=None):
+    return temp('passwd.html', title="Password Checker", test=test)
+
+@app.route('/passwd/')
 def passwd_index():
+    passwd = request.args.get('passwd', False)
+    if passwd:
+        return redirect('/passwd/' + passwd)
     return passwd_temp()
 
+@app.route('/passwd/<path:passwd>')
+def passwd_check(passwd=None):
+    if passwd:
+        test = pwm.test(passwd)
+        return passwd_temp(test)
+    else:
+        return redirect('/passwd')
+
+@app.context_processor
+def utility_processor():
+    def format_score(score):
+        return "{0:.02f}%".format(score * 100)
+    return dict(format_score=format_score)
